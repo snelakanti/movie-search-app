8eb7c64a60b04c8ffb6f8823027b62e917e91e36
snelakanti <vnelakanti@gmail.com>
2018-06-04 21:03:58 -0400

    create search.ejs

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req,res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=harry&apikey=thewdb", function(error,response,body){
 if(!error && response.statusCode == 200){
