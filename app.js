9883709064bd61c00105a9dc4ad766a7f30ef50a
snelakanti <vnelakanti@gmail.com>
2018-06-04 20:46:49 -0400

    test if api data is fetched

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var request = require("request");
 app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=harry&apikey=thewdb", function(error,response,body){
 if(!error && response.statusCode == 200){
-    res.send(body);
+    var results = JSON.parse(body);
+    res.send(results["Search"][0]["Title"]);
 }
     });
 })
