d4987b0f782622bda4c277e2cd14c4d5a4662b4d
snelakanti <vnelakanti@gmail.com>
2018-06-04 21:06:15 -0400

    connect search template to search route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
+app.get("/search", function(req,res){
     res.render("search");
 });
 
