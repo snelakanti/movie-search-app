cb297b9603a6ab5e779c40bccb0c021be774abc6
snelakanti <vnelakanti@gmail.com>
2018-06-04 20:49:12 -0400

    create results.ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=harry&apikey=thewdb", function(error,response,body){
